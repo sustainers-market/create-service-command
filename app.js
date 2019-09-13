5d586b685e0dafdfe1ec5a7c4137d757dde1577d
Joao <ritterjv@gmail.com>
2019-09-12 23:03:27 -0700

    -

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const app = express();
+
+const logger = require("@sustainers/logger");
+const middleware = require("@sustainers/middleware");
+
+const errorHandler = require("./src/error_handler");
+
+cors(app);
+middleware(app);
+
+app.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    logger.info("Request: ", {
+      params: req.params,
+      body: req.body,
+      query: req.query,
+      headers: req.headers
+    });
+    res.send("🍉");
+  })
+);
+
+errorHandler(app);
+
+module.exports = app;
